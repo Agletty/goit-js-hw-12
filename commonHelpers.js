74e64bed8409657f7fe19f80a96f9953f830cc7a
Agletty <141568532+Agletty@users.noreply.github.com>
2024-01-26 00:43:47 +0000

    Deploying to gh-pages from @ Agletty/goit-js-hw-12@179fcfb6083d2abf1538f5f339cda94cbe3cadeb 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
-import{S as p,i as m}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const h="https://pixabay.com/api/",y="41902391-3586b75e6bf6b946d25386040",g=document.querySelector(".search-form"),s=document.querySelector('[name="query"]'),l=document.querySelector(".gallery"),c=document.querySelector(".loader"),b=new p(".gallery a",{captionsData:"alt",captionDelay:250});u();g.addEventListener("submit",L);function L(n){n.preventDefault();const r=s.value.trim();if(r==="")return;v();const a=`${h}?key=${y}&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`;S(a).then(o=>{o.hits.length===0?m.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):$(o.hits)}).catch(o=>{console.error(o)})}function S(n){return q(),fetch(n).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()}).finally(()=>{u(),s.value="",s.focus()})}function $(n){const r=n.map(({webformatURL:a,largeImageURL:o,tags:e,likes:t,views:i,comments:d,downloads:f})=>`
+import{S as L,a as S,i as d}from"./assets/vendor-bad0427b.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function a(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=a(e);fetch(e.href,r)}})();const v="41902391-3586b75e6bf6b946d25386040",w="https://pixabay.com/api/",$=document.querySelector(".search-form"),u=document.querySelector('[name="query"]'),f=document.querySelector(".gallery"),p=document.querySelector(".loader"),c=document.querySelector(".load-more-btn");let i,h="";const q=new L(".gallery a",{captionsData:"alt",captionDelay:250});y();l();$.addEventListener("submit",M);c.addEventListener("click",P);async function M(o){o.preventDefault();const t=u.value.trim();t!==""&&(E(),l(),h=t,i=1,await m())}async function P(){i+=1,await m(),A()}async function m(){B();const o=`${w}?key=${v}&q=${h}&image_type=photo&orientation=horizontal&safesearch=true&page=${i}&per_page=40`;try{const t=await S.get(o);if(t.data.hits.length===0)d.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"});else{k(t.data.hits);const a=t.data.totalHits,n=t.data.hits.length;i*n>=a?(l(),d.info({title:"End of results",message:"We’re sorry, but you’ve reached the end of search results."})):_()}}catch(t){console.error(t)}finally{y(),u.value=""}}function k(o){const t=o.map(({webformatURL:a,largeImageURL:n,tags:e,likes:r,views:s,comments:g,downloads:b})=>`
       <div class="photo-card">
-        <a href="${o}" data-lightbox="gallery" data-title="${e}">
+        <a href="${n}" data-lightbox="gallery" data-title="${e}">
           <img
             class="photo-card__img"
             src="${a}"
@@ -12,18 +12,18 @@ import{S as p,i as m}from"./assets/vendor-46aac873.js";(function(){const r=docum
         </a>
         <div class="info">
           <p class="info-item">
-            <b>Likes:</b> <span>${t}</span>
+            <b>Likes:</b> <span>${r}</span>
           </p>
           <p class="info-item">
-            <b>Views:</b> <span>${i}</span>
+            <b>Views:</b> <span>${s}</span>
           </p>
           <p class="info-item">
-            <b>Comments:</b> <span>${d}</span>
+            <b>Comments:</b> <span>${g}</span>
           </p>
           <p class="info-item">
-            <b>Downloads:</b> <span>${f}</span>
+            <b>Downloads:</b> <span>${b}</span>
           </p>
         </div>
       </div>
-      `).join("");l.insertAdjacentHTML("beforeend",r),b.refresh()}function v(){l.innerHTML=""}function q(){c.style.display="block"}function u(){c.style.display="none"}
+      `).join("");f.insertAdjacentHTML("beforeend",t),q.refresh()}function E(){f.innerHTML=""}function B(){p.style.display="block"}function y(){p.style.display="none"}function _(){c.style.display="block"}function l(){c.style.display="none"}function x(o){return o?o.getBoundingClientRect().height:0}function H(o){window.scrollBy({top:o,behavior:"smooth"})}function A(){const o=document.querySelector(".photo-card");H(x(o)*2)}
 //# sourceMappingURL=commonHelpers.js.map
